18d64dd186ada1cf5952f57dd720905bfd6cca00
Muhammad Muslay Uddin Rony <121866087+M-U-Rony@users.noreply.github.com>
2025-01-08 10:12:02 +0600

    Delete Challenge-4/index.ts

--- Challenge-4/index.ts
+++
@@ -1,6 +0,0 @@
-import { films } from '/data.js'
-
-// Some useful elements
-const guessInput = document.getElementById('guess-input')
-const messageContainer = document.getElementsByClassName('message-container')[0]
-const emojiCluesContainer = document.getElementsByClassName('emoji-clues-container')[0]
